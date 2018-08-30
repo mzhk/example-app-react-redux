95f97787a10e6e67cacb0b56f4d9f65aa95a9c91
mzhk <mariusz.hanzlik@gmail.com>
2018-08-30 16:46:29 +0200

    app init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # example-app-react-redux
+Minimal example app - react, redux, babel 7, webpack 4
+
+## Usage
+
+     git clone https://github.com/mzhk/example-app-react-redux.git
+     cd example-app-react-redux
+     npm install
+
+
+## Dev mode 
+
+    npm run start
+    open http://localhost:8080/
+  
+
+## Build 
+
+    npm run build
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
